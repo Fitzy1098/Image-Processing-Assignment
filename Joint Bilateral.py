d34f4f390fbd0ec416862319dc7495dec556b2ca
Fitzy1098 <44231612+Fitzy1098@users.noreply.github.com>
2018-12-02 16:15:58 +0000

    Add files via upload

--- Joint Bilateral.py
+++ Joint Bilateral.py
@@ -1,91 +1,74 @@
 import cv2
 import numpy
 
-def gauss(x,sigma):
-    return (1.0/(2*numpy.pi*(sigma**2)))*numpy.exp(-(x**2)/(2*(sigma**2)))      #Applies the Gaussian function
+def gauss(x, sigma):
+    gaussianFunction=(1.0/(((2*numpy.pi)**0.5)*sigma))*numpy.exp(-(x**2)/(2*(sigma**2)))      #Applies the Gaussian function
+    return gaussianFunction
 
-def distance(x1,y1,x2,y2):
-    return numpy.sqrt(numpy.abs((x1-x2)**2-(y1-y2)**2))     #Calculates the distance between 2 pixels
+def distance(x1, y1, x2, y2):
+    distance=numpy.sqrt(numpy.abs(((x1-x2)**2)+((y1-y2)**2)))     #Calculates the distance between 2 pixels
+    return distance
 
+def distance2(diameter,sigma):          #Creates a list of gaussian function values for pixels in the neighbourhood
+    middle=(diameter+1)/2               #Finds co-ordinates for centre of neighbourhood 
+    middle-=1
+    gausses=[]
+    for i in range(diameter):           #Iterates through all pixel co-ordinates in neighbourhood
+        for j in range(diameter):
+            dist=distance(i, j, middle, middle)         #Calculates distance between centre and current co-ordinates
+            g=gauss(dist, sigma)                        #Calculates the gaussian function 
+            gausses.append(g)               #Appends the value to the list    
+    return gausses
+           
+    
+    
 def joint_bilateral_filter(image, image1, output, diameter, intensity_sigma, spatial_sigma):  #Applies the joint bilaeral filter to the 2 images    
-
-    for row in range(len(image)):       #Iterates through the rows of image
-        for column in range(len(image[0])):    #Iterates through the columns of image
-            for z in range(0,2):                #Iterates through the 3 colour channels
+    radius=diameter/2
+    numRows=len(image)
+    numColumns=len(image[0])
+    gausses=distance2(diameter, spatial_sigma)
+    for row in range(numRows):       #Iterates through the rows of image
+        for column in range(numColumns):    #Iterates through the columns of image
+            for z in range(0,3):                #Iterates through the 3 colour channels
                 ks=0                            #Normalisation factor
                 filtered=0                      #Output for pixel channel
+                point=0
                 for i in range(diameter):       #Iterates through the neighbourhood 
                     for j in range(diameter):
-                        near_x=int(row-(diameter/2-i))      #Finds x co-ordinate of a pixel in the neighbourhood
-                        near_y=int(column-(diameter/2-j))   #Finds x co-ordinate of a pixel in the neighbourhood
-                        if near_x>=len(image):              #Adjusts the x co-ordinate if it is outside the range of the image
-                            near_x-=len(image)
-                        if near_y>=len(image[0]):           #Adjuts the y co-ordinate if it is outside the range of the image
-                            near_y-=len(image[0])
-
-                        gauss_intensity = gauss(image1[near_x][near_y][z]-image1[row][column][z], intensity_sigma) #Calculate the Gaussian function on the difference in intensities of the two pixels
-                        gauss_spatial = gauss(distance(near_x, near_y, row, column), spatial_sigma)                 #Calcualtes the Gaussian function on the distance between the pixels
+                        near_x=int(row-(radius-i))      #Finds x co-ordinate of a pixel in the neighbourhood
+                        near_y=int(column-(radius-j))   #Finds x co-ordinate of a pixel in the neighbourhood
+                        if near_x>=numRows:              #Adjusts the x co-ordinate if it is outside the range of the image
+                            near_x-=numRows
+                        if near_y>=numColumns:           #Adjuts the y co-ordinate if it is outside the range of the image
+                            near_y-=numColumns
+                        gauss_spatial=gausses[point]
+                        gauss_intensity = gauss(int(image1[near_x][near_y][z])-int(image1[row][column][z]), intensity_sigma) #Calculate the Gaussian function on the difference in intensities of the two pixels
+                        #gauss_spatial = gauss(distance(near_x, near_y, row, column), spatial_sigma)                 #Calcualtes the Gaussian function on the distance between the pixels
                         k=gauss_spatial*gauss_intensity         #Multipliy the two functions togther
                         filtered+=(image[near_x][near_y][z]*k)      #Sum together the functions multiplied by the intensity value for the channel of the pixel in the original image
                         ks+=k                                   #Sum together the functions
-                filtered=filtered // ks                         #Divide by the normalisation factor and round to an integer
+                        point+=1
+                filtered=filtered//ks                         #Divide by the normalisation factor and round to an integer
                 output[row][column][z]=filtered  #Set the pixel channel value to the new value rounded to the nearest whole number
-    return output
+    return output  #Return the filtered image
 
 def run():
     windowName="Smoothed Image"     #Names the display window
 
-    image=cv2.imread('./test3a.jpg',cv2.IMREAD_COLOR)       #Reads the ambient image
-    image1=cv2.imread('./test3b.jpg',cv2.IMREAD_COLOR)      #Reads the flash image
+    image=cv2.imread('./test3a.jpg', cv2.IMREAD_COLOR)       #Reads the ambient image
+    image1=cv2.imread('./test3b.jpg', cv2.IMREAD_COLOR)      #Reads the flash image
 
-    if not image is None and not image1 is None:
-        #filtered_image_OpenCV = cv2.bilateralFilter(image, 7, 20.0, 20.0)
-        #cv2.imwrite("image.png",image)
-        output=image.copy()
-        #img_own = bilateral_filter(image, image1, output, 15, 35, 35)
-        #cv2.imwrite("filtered_1.jpg", img_own)
-        #output=image.copy()
-        
-        #img_own = bilateral_filter(image, image1, output, 7, 10, 10)
-        #cv2.imwrite("filtered_3.jpg", img_own)
-        #output=image.copy()
+    if not image is None and not image1 is None:        #Checks if both files are images
+        output=image.copy()                             #Creates a copy of the input image to append the values to
+        img_own = joint_bilateral_filter(image, image1, output, 13, 2, 3) #ambient image, flash image, the output image, the diameter of the neighbourhood, intensity sigma, spatial sigma
+        cv2.imwrite("filtered_image.jpg", img_own)       #Writes the image to a file
 
-        #img_own = bilateral_filter(image, image1, output, 20, 20, 20)
-        #cv2.imwrite("filtered_4.jpg", img_own)
-        #output=image.copy()
-        
-        #img_own = bilateral_filter(image, image1, output, 7, 20, 20) #ambient image, flash image, the output image, the diameter of the neighbourhood, intensity sigma, spatial sigma
-        #cv2.imwrite("filtered_test2.jpg", img_own)
-
-        #img_own = joint_bilateral_filter(image, image1, output, 7, 10, 30) #ambient image, flash image, the output image, the diameter of the neighbourhood, intensity sigma, spatial sigma
-        #cv2.imwrite("filtered1.jpg", img_own)
-        
-        img_own = joint_bilateral_filter(image, image1, output, 15, 10, 40) #ambient image, flash image, the output image, the diameter of the neighbourhood, intensity sigma, spatial sigma
-        cv2.imwrite("filtered2.jpg", img_own)
-        
-        cv2.imshow(windowName, img_own)
+        cv2.imshow(windowName, img_own)     #Displays the filtered image in a window
         key = cv2.waitKey(0)
-        if (key == ord('x')):
+        if (key == ord('x')):               #Closes the window if the 'x' key is pressed
             cv2.destroyAllWindows()
     else:
-        print("No image file successfully loaded.")
+        print("An image file did not successfully load.")     #Prints this statement if one or both images fail to load
     
-    
-
-def hue(pixel):
-    hue=0
-    RGB=[]
-    RGB[0]=pixel[0]/255 #B
-    RGB[1]=pixel[1]/255 #G
-    RGB[2]=pixel[2]/255 #R
-    if RGB[2]>=RGB[1] and RGB[2]>=RGB[0]:
-        hue=(RGB[1]-RGB[0])/(max(RGB)-min(RGB))
-    if RGB[1]>=RGB[2] and RGB[1]>=RGB[0]:
-        hue=2.0+(RGB[0]-RGB[2])/(max(RGB)-min(RGB))
-    if RGB[0]>=RGB[2] and RGB[0]>=RGB[1]:
-        hue=4.0+(RGB[2]-RGB[1])/(max(RGB)-min(RGB))
-    hue*=60
-    if hue<0:
-        hue+=360
-    return hue
+run() #Runs the program
     
